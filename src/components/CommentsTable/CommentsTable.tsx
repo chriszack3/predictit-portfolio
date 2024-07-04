a6aff9b74c6066b779781286dab374f9adc22964
chriszack3 <czack@chrispc>
2024-07-04 16:07:14 -0400

    build: added filtering by pos, neg, neutral

--- src/components/CommentsTable/CommentsTable.tsx
+++ src/components/CommentsTable/CommentsTable.tsx
@@ -1,5 +1,6 @@
-import CommentPill from './CommentPill/CommentPill';
+import { useState } from 'react';
 
+import CommentPill from './CommentPill/CommentPill';
 import { getPosCount, getNegCount, getNeutCount } from '@/constants/functions';
 
 import { Post } from '@/constants/interfaces';
@@ -8,6 +9,8 @@ type CommentTableProps = {
   allComments: Array<Post>;
 };
 
+type Filter = 'positive' | 'negative' | 'neutral';
+
 export default function CommentTable({ allComments }: CommentTableProps) {
   const meta = {
     commentCount: allComments.length,
@@ -15,15 +18,54 @@ export default function CommentTable({ allComments }: CommentTableProps) {
     negativeCount: getNegCount(allComments),
     neutralCount: getNeutCount(allComments),
   };
+
+  const [filters, setFilters] = useState<Array<Filter>>([
+    `positive`,
+    `negative`,
+    `neutral`,
+  ]);
+  const arrToRender = allComments.filter((comment) => {
+    const score = comment?.result?.documentSentiment?.score || 0;
+    if (score >= 0.25) {
+      return filters.includes(`positive`);
+    }
+    if (score <= -0.25) {
+      return filters.includes(`negative`);
+    }
+    if (score <= 0.25 && score >= -0.25) {
+      return filters.includes(`neutral`);
+    } else {
+      return false;
+    }
+  });
+  const handleClickCheck = (filtStr: Filter, filters: Array<Filter>) => {
+    !filters.includes(filtStr)
+      ? setFilters([...filters, filtStr])
+      : setFilters(filters.filter((f) => f !== filtStr));
+  };
   return (
     <div>
-      <h5>Meta</h5>
-      <p># of Positive Comments: {meta.positiveCount}</p>
-      <p># of Negative Comments: {meta.negativeCount}</p>
-      <p># of Neutral Comments: {meta.neutralCount}</p>
+      <h5>Meta</h5># of Positive Comments: {meta.positiveCount}
+      <input
+        defaultChecked={true}
+        type="checkbox"
+        onClick={() => handleClickCheck(`positive`, filters)}
+      ></input>
+      # of Negative Comments: {meta.negativeCount}
+      <input
+        defaultChecked={true}
+        type="checkbox"
+        onClick={() => handleClickCheck(`negative`, filters)}
+      ></input>
+      # of Neutral Comments: {meta.neutralCount}
+      <input
+        defaultChecked={true}
+        type="checkbox"
+        onClick={() => handleClickCheck(`neutral`, filters)}
+      ></input>
       <div className="flex flex-wrap w-3/12">
-        {allComments.length > 0 &&
-          allComments.map((comment, i) => {
+        {arrToRender.length > 0 &&
+          arrToRender.map((comment, i) => {
             return (
               <span key={i}>
                 <CommentPill post={comment} />
